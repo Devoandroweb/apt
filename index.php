3291debf7b1efea1da0e421ad17a508e3256adf4
fathur <syafi.arrosyid@gmail.com>
2022-01-28 20:09:27 +0700

    a

---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>APT</title>
+    <link rel="stylesheet" href="boostrap/css/bootstrap.min.css">
+    <link rel="stylesheet" href="custom.css">
+</head>
+
+<body>
+    <section class="bg-black section-top">
+        <div class="content text-white text-center">
+            <p>optimasikan wherehouse anda dengan teknologi</p>
+            <H1 style="font-weight: 600;">WAREHOUSE MANAGEMENT SYSTEM</H1>
+            <p>Memudahkan seluruh akrifitas operasional warehouse, <br>tersedia dalam platform web dan mobile dengan teknologi baru </p>
+            <a href="#" class="btn btn-white fw-bold mt-5">Konsultasi Sekarang</a>
+        </div>
+    </section>
+    <section class="bg-light">
+        <div class="row mx-auto p-5">
+            <div class="col-md-5">
+                <div class="embed-video bg-black">
+                    <embed src="" type="">
+                </div>
+            </div>
+            <div class="col-md-7">
+                <div>
+                    <h4>Deskripsi</h4>
+                    <p>Warehouse Management System adalah layanan sistem yang berkaitan dengan pengelolaan pergerakan keluar masuknya barang ke dalam gudang. Aktivitas Warehouse Management System berkaita dengan</p>
+                </div>
+                <div class="mt-5">
+                    <h4>Manfaat</h4>
+
+                    <ul class="fa-ul">
+                        <li><span class="fa-li"><i class="fas fa-check-square"></i></span>List icons can</li>
+                        <li>mempercepat proses lead time secara komputasi / otomatis</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </section>
+
+
+
+
+    <script src="boostrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
